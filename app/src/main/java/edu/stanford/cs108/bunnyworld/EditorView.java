0f7d6bdd01b779ee852f593443ffcba52a09172c
yuwenzhang <ivy_ywzhang@outlook.com>
2017-11-27 21:27:42 -0800

    Finish some shape drawing funcitons

--- app/src/main/java/edu/stanford/cs108/bunnyworld/EditorView.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/EditorView.java
@@ -57,6 +57,7 @@ public class EditorView extends View {
 
     public void drawText(BunnyShape shape) {
         String textString = shape.getTextString();
+        String current;
         //canvas.drawText("textString",);
 
     }
