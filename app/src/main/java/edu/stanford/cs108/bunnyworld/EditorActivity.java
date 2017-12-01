038fb76225cf78e8bae166abe96f52a931eb60d5
yuwenzhang <ivy_ywzhang@outlook.com>
2017-11-30 20:19:48 -0800

    Near to success!

--- app/src/main/java/edu/stanford/cs108/bunnyworld/EditorActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/EditorActivity.java
@@ -13,6 +13,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -33,22 +34,17 @@ public class EditorActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+    View view;
 
     public void onCreateNewPage(View view) {
-        EditorView editorView = (EditorView) findViewById(R.id.editorView);
-        editorView.createNewPage();
-        initiatePopupWindow(view);
-//        currentPage = editorView.currentPage;
-//        selected = editorView.selectedShape;
-
-
+        popupWindow1(view);
+        this.view = view;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         EditorView editorView = (EditorView) findViewById(R.id.editorView);
-//        editorView.createNewPage();
-//        initiatePopupWindow(view);
+
         currentPage = editorView.currentPage;
         selected = currentPage.selectedShape;
         // Handle item selection
@@ -59,6 +55,9 @@ public class EditorActivity extends AppCompatActivity {
                     Log.i("null", "null");
                 } else {
                     selected.setSelectScript("GoTo");
+
+                    popupWindow2(view);
+
                     Log.i(selected.getName(), selected.getSelectScript());
                 }
 
@@ -94,9 +93,7 @@ public class EditorActivity extends AppCompatActivity {
 
 
     public void setString() {
-        EditorView editorView = (EditorView) findViewById(R.id.editorView);
-        currentPage = editorView.currentPage;
-        selected = currentPage.selectedShape;
+
 
     }
 
@@ -116,23 +113,93 @@ public class EditorActivity extends AppCompatActivity {
         }
 
     }
-
-    private void initiatePopupWindow(View v) {
+    private void popupWindow1(View v) {
         try {
-            //We need to get the instance of the LayoutInflater, use the context of this activity
             LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View layout = inflater.inflate(R.layout.popup_window,null);
+
+            //We need to get the instance of the LayoutInflater, use the context of this activity
+
             //Inflate the view from a predefined XML layout
-            final View layout = inflater.inflate(R.layout.popup_window,
-                    null);
-            // create a 300px width and 470px height PopupWindow
-            final PopupWindow pw = new PopupWindow(layout, 300, 470, true);
+
+            final PopupWindow pw = new PopupWindow(layout, 800, 350, true);
             // display the popup in the center
             pw.showAtLocation(v, Gravity.CENTER, 0, 0);
-            layout.setOnTouchListener(new View.OnTouchListener() {
+
+            Button enterBtn = (Button) layout.findViewById(R.id.createPageEnter_button);
+            Button cancelBtn = (Button) layout.findViewById(R.id.createPageCancel_button);
+
+            cancelBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public boolean onTouch(View view, MotionEvent event) {
+                public void onClick(View view) {
                     pw.dismiss();
-                    return true;
+                }
+            });
+
+
+            enterBtn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    EditText editText = (EditText) layout.findViewById(R.id.pageName_text);
+                    String newPageName = editText.getText().toString();
+                    EditorView editorView = (EditorView) findViewById(R.id.editorView);
+                    editorView.createNewPage(newPageName);
+
+                    editText.setText("");
+                    pw.dismiss();
+
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void popupWindow2(View v) {
+        try {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View layout = inflater.inflate(R.layout.popup_window2,null);
+
+            //We need to get the instance of the LayoutInflater, use the context of this activity
+
+            //Inflate the view from a predefined XML layout
+
+            final PopupWindow pw = new PopupWindow(layout, 800, 350, true);
+            // display the popup in the center
+            pw.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+            Button enterBtn = (Button) layout.findViewById(R.id.goToPageEnter_button);
+            Button cancelBtn = (Button) layout.findViewById(R.id.goToPageCancel_button);
+
+            cancelBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    pw.dismiss();
+                }
+            });
+
+
+            enterBtn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    EditText editText = (EditText) layout.findViewById(R.id.page_text);
+                    String goToPage = editText.getText().toString();
+                    selected.setSelectScript(selected.getSelectScript() + goToPage);
+                    editText.setText("");
+                    Log.i(selected.getName(), selected.getSelectScript());
+                    //This is used to change page which can be used during game
+                    /*
+                    EditorView editorView = (EditorView) findViewById(R.id.editorView);
+                    editorView.openPage(goToPage);
+                    */
+                    pw.dismiss();
+
+
                 }
             });
 
